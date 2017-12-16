fb397f2ced4a487dc4f99859bca076dcc211e865
vineet <vineet.harkut@signure.com>
2017-12-16 23:34:36 +0530

    Inital Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} ${req.url}` ;
+	fs.appendFile('server.log',log + '\n',(err) => {
+		if(err){
+			console.log('Unable to append to server.log')			
+		}
+	});
+	next();
+});
+
+app.use((req,res,next) => {
+	res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear',() => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('hello',() => {
+	return "Hello" ;
+});
+
+app.get('/',(req,res) => {
+	//res.send('<h1>Hello World</h1>') ;
+	/*res.send({
+		name:'Vineet',
+		hobby:[
+			'Cricket',
+			'Music',
+			'Programming'
+		]
+	})*/
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to Home Page'
+	});
+});
+
+app.get('/about',(req,res) => {
+	//res.send('About Us Page')
+	res.render('about.hbs',{
+		pageTitle:'About Page'
+	});
+});
+
+app.get('/bad',(req,res) => {
+	res.send({
+		errorMessage:'404 Not Found'
+	});
+})
+
+app.listen(3000);
