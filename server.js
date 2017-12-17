3a1d7c33fbf9261437a169c61a0b95e2d42f674e
vineet <vineet.harkut@signure.com>
2017-12-17 09:59:26 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000 ;
 
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
@@ -10,6 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use((req,res,next) => {
 	var now = new Date().toString();
 	var log = `${now} : ${req.method} ${req.url}` ;
+	console.log(log);
 	fs.appendFile('server.log',log + '\n',(err) => {
 		if(err){
 			console.log('Unable to append to server.log')			
@@ -18,9 +20,9 @@ app.use((req,res,next) => {
 	next();
 });
 
-app.use((req,res,next) => {
+/*app.use((req,res,next) => {
 	res.render('maintenance.hbs');
-});
+});*/
 
 hbs.registerHelper('getCurrentYear',() => {
 	return new Date().getFullYear()
@@ -59,4 +61,6 @@ app.get('/bad',(req,res) => {
 	});
 })
 
-app.listen(3000);
+app.listen(port,() => {
+	console.log('Server is up on port '+port)
+});
